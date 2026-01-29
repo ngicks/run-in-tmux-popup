817f0e1feac528bfe66e35d161f29613203d4098
ngicks <yknt.bsl@gmail.com>
2026-01-30 03:26:33 +0900

    ✨: add json

--- internal/pickentry/callback.go
+++ internal/pickentry/callback.go
@@ -2,15 +2,28 @@ package pickentry
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"text/template"
 )
 
+var FuncMap = template.FuncMap{
+	"json": func(v any) (string, error) {
+		bin, err := json.Marshal(v)
+		if err != nil {
+			return "", err
+		}
+		return string(bin), nil
+	},
+}
+
 // RenderCallback renders a callback template with the given item.
 func RenderCallback(tmpl string, item Item) (string, error) {
-	t, err := template.New("callback").Parse(tmpl)
+	t, err := template.New("callback").
+		Funcs(FuncMap).
+		Parse(tmpl)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse callback template: %w", err)
 	}
